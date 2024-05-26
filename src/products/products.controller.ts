4f253d24b93bbfc20d47228ed2fb0a698171f123
erickmaycolhc <erickmaycolhc@gmail.com>
2024-05-26 02:50:17 -0400

    feat: se creó petición de producto por id

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductDto } from './dto/product';
 
@@ -9,5 +9,10 @@ export class ProductsController {
   @Get()
   findAllProducts(): Promise<ProductDto[]> {
     return this.productsService.findAllProducts();
+  }
+
+  @Get(':id')
+  findByIdProduct(@Param('id') id: string) {
+    return this.productsService.findByIdProduct(id);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -22,7 +22,9 @@ export class ProductsService {
               ecommerce_products.date_registered,
               ecommerce_products.url
       FROM ecommerce_products
-      INNER JOIN ecommerce_categories ON ecommerce_categories.id = ecommerce_products.id_category`,
+      INNER JOIN ecommerce_categories ON ecommerce_categories.id = ecommerce_products.id_category
+      where ecommerce_products.id = 1
+      `,
     );
 
     let productsFinally: ProductDto[] = await Promise.all(
@@ -35,6 +37,7 @@ export class ProductsService {
         });
 
         return {
+          id: item.id,
           name: item.name,
           category: {
             name: item.category_name,
@@ -59,5 +62,52 @@ export class ProductsService {
       [id],
     );
     return images;
+  }
+
+  async findByIdProduct(id: string) {
+    const resultQuery = await this.dataSource.query(
+      `SELECT 
+      ecommerce_products.id,
+      ecommerce_products.name,
+          ecommerce_categories.name 'category_name',
+              ecommerce_categories.url 'category_url',
+              ecommerce_products.price,
+              ecommerce_products.description,
+              ecommerce_products.date_registered,
+              ecommerce_products.url
+      FROM ecommerce_products
+      INNER JOIN ecommerce_categories ON ecommerce_categories.id = ecommerce_products.id_category
+      where ecommerce_products.id = ?
+      `,
+      [id],
+    );
+
+    let productsFinally: ProductDto[] = await Promise.all(
+      resultQuery.map(async (item) => {
+        const images = await this.findImages(item.id);
+        images.map((itemImage) => {
+          return {
+            url: itemImage.image,
+          };
+        });
+
+        return {
+          id: item.id,
+          name: item.name,
+          category: {
+            name: item.category_name,
+            url: item.category_url,
+          },
+          images: images,
+          price: item.price,
+          description: item.description,
+          date_registered: item.date_registered,
+          url: item.url,
+        };
+      }),
+    );
+
+    console.log('productsFinally=>>', productsFinally);
+    return productsFinally;
   }
 }
